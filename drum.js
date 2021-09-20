2b42b08f7eaa2c3a4ea636bd2920cb4df8addbb8
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-21 00:11:18 +0530

    continue

--- drum.js
+++ drum.js
@@ -4,5 +4,13 @@ window.addEventListener('keydown', function(e){
     if(!audio)return;
     audio.currentTime = 0;
     audio.play();
-    console.log(key)
+    key.classList.add('playing');
 });
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform')return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitioned',removeTransition));
