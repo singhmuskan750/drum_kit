ecbbcdcf7b1d23deaada5eadb9cf8af0e9fc6b0b
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-21 01:04:19 +0530

    drum_kit complete

--- drum.js
+++ drum.js
@@ -1,11 +1,11 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const key =document.querySelector(`.key[data-key = "${e.keyCode}"]`);
-    if(!audio)return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-});
+function playsound(e){
+ const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+ const key =document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+ if(!audio)return;
+ audio.currentTime = 0;
+ audio.play();
+ key.classList.add('playing');
+}
 
 function removeTransition(e){
     if(e.propertyName !== 'transform')return;
@@ -13,4 +13,5 @@ function removeTransition(e){
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned',removeTransition));
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown', playsound);
